f4089ba08f7274addad19f499ebdc461996ffad6
dtr6210 <rialsdean@gmail.com>
2023-12-10 16:40:43 -0600

    added swagger documentation

--- public/fakestore.js
+++ public/fakestore.js
@@ -7,14 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
   //Function to fetch data by category
   function fetchData(category) {
     //clear existing content in the postContainer
-    postContainer.innerHTML = '';
+    postContainer.innerHTML = "";
     axios
-      .get(`https://fakestoreapi.com/products/category/${category}`)
+      .get(`https://fakestoreapi.com/products/${category}`)
       .then((response) => {
         console.log(response.data);
-        const products = Array.isArray(response.data) ? response.data: [];
+        const products = Array.isArray(response.data) ? response.data : [];
 
-     
         products.forEach((product) => {
           const card = document.createElement("div");
           card.className = "card";
@@ -52,11 +51,11 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   //Event listener for the select element
-  categoryDropdown.addEventListener('change', function () {
+  categoryDropdown.addEventListener("change", function () {
     const selectedCategory = categoryDropdown.value;
     console.log("Selected Category:", selectedCategory);
     fetchData(selectedCategory);
   });
-  
+
   fetchData(categoryDropdown.value);
 });
